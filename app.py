cf62c518b07113ac64cd7bbe97734de5d785006d
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2023-03-05 19:54:54 +0530

    first commit

--- app.py
+++ app.py
@@ -34,6 +34,30 @@ def Summarize():
 	else:
 		return render_template("index.html")
 
+@app.route("/summary", methods=["GET", "POST"])
+def Summary():
+	if req.method == "POST":
+		API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+		headers = {"Authorization": f"Bearer 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"}
+
+		data = req.json["article"]
+
+		maxL = int(req.json["maxL"])
+		minL = maxL // 4
+
+		def query(payload):
+			response = requests.post(API_URL, headers=headers, json=payload)
+			return response.json()
+
+		output = query({
+			"inputs": data,
+			"parameters": {"min_length": minL, "max_length": maxL},
+		})[0]
+
+		return output["summary_text"]
+	else:
+		return 0
+
 	
 if __name__ == '__main__':
 	app.run(debug=True)
